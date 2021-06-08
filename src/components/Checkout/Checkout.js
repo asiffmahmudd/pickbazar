d07df2b658449cee92ca01cda9773ce2d8b393b0
Asif Mahmud <asifmahmud3472@gmail.com>
2021-06-08 22:36:15 +0600

    radio card added

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -8,18 +8,20 @@ const Checkout = () => {
             <Header></Header>
 
             <div className="checkout container">
+                <form>
                     <div className="row mt-5">
                         <div className="col-md-9 p-lg-5 p-md-4 p-sm-4 p-3 mx-auto checkout-wrapper rounded">
-                            <div className="checkout-overview p-lg-5 p-md-4 p-sm-4 p-3">
-                                <div className="sub-total overview-item">
+
+                            <div className="order-summary p-lg-5 p-md-4 p-sm-4 p-3">
+                                <div className="sub-total summary-item">
                                     <span>Sub Total (1 items)</span>
                                     <span>$1.50</span>
                                 </div>
-                                <div className="shipping overview-item">
+                                <div className="shipping summary-item">
                                     <span>Shipping fee</span>
                                     <span>$1.50</span>
                                 </div>
-                                <div className="voucher overview-item">
+                                <div className="voucher summary-item">
                                     <span>Voucher</span>
                                     <span>
                                         <form>
@@ -30,13 +32,45 @@ const Checkout = () => {
                                         </form>
                                     </span>
                                 </div>
-                                <div className="sub-total overview-item">
+                                <div className="sub-total summary-item">
                                     <span>Total</span>
                                     <span>$1.50</span>
                                 </div>
                             </div>
+
+                            <div className="delivery-schedule">
+                                <h3 className="section-header">Delivery Schedule</h3>
+                                <div className="radio-group">
+                                    <div className="row">
+                                        <div className="col-md-4">
+                                            <label>
+                                                <input type="radio" name="product" class="card-input-element" />
+                                                <div class="panel panel-default card-input">
+                                                    <div class="panel-heading">Product B</div>
+                                                    <div class="panel-body">
+                                                        Product specific content
+                                                    </div>
+                                                </div>
+                                            </label>
+                                        </div>
+                                        <div className="col-md-4">
+                                            <label>
+                                                <input type="radio" name="product" class="card-input-element" />
+                                                <div class="panel panel-default card-input">
+                                                    <div class="panel-heading">Product B</div>
+                                                    <div class="panel-body">
+                                                        Product specific content
+                                                    </div>
+                                                </div>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
                         </div>
                     </div>
+                </form>
             </div>
         </>
     );
